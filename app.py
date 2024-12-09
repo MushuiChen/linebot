83bcceda71a7d2247b1c45ee0547b0718612b1c7
MushuiChen <a0929071145@gmail.com>
2024-12-09 17:53:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import random
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('wvdgugGq+zaBg3f3hJL+Tsjg/TxkBxYnrfm0w/cEXS7LEUK0uNH+eeNwOx8mC55udDDDBp6Iy+6PA3CUILWH4D6O8DhmMDe/cdio/MSvcBp6Iy+6PA3CUILWH4D6O8DhmMDe/cC/MSvcBp6Iy+6PA3CUILWH4D6O8DhmMDe/cC/MSvcBp6Iy+LnnvL0FFFyg700FpFm/MS B04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('wvdgugGq+zaBg3f3hJL+Tsjg/TxkBxYnrfm0w/cEXS7LEUK0uNH+eeNwOx8mC55udDvcBp6Iy+6PA3CUILWH4D6O8DhmMDe/cdio/MS9Kio+5ovLnHM7w0wNek7dL0e0nZpLgM4X+LVlrgMZvXEgmgdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('97a38974ee2dad9c5f575f82ce28a7c0')
 
@@ -50,54 +50,54 @@ def handle_message(event):
 
     elif message == '心情好':
             sticker_message = StickerSendMessage(
-            package_id='8515',
-            sticker_id='16581242'  # 開心的貼圖
+            package_id='446',
+            sticker_id='1991'  # 開心的貼圖
         )
             line_bot_api.reply_message(event.reply_token, sticker_message)
 
     elif message == '心情不好':
             sticker_message = StickerSendMessage(
-            package_id='8515',
-            sticker_id='16581262'  # 傷心的貼圖
+            package_id='11539',
+            sticker_id='52114138'  # 傷心的貼圖
         )
             line_bot_api.reply_message(event.reply_token, sticker_message)
 
     elif message == '找美食':
             location_message = LocationSendMessage(
-            title='吃光食堂',
-            address='公民里 台中市西區',
-            latitude=24.142004059652407,
-            longitude=120.6708091028049
+            title='著名餐廳',
+            address='Hog Island Oyster Co.',
+            latitude=37.79726181895184,
+            longitude=-122.39362601431256
         )
             line_bot_api.reply_message(event.reply_token, location_message)
 
     elif message == '找景點':
             location_message = LocationSendMessage(
-            title='湖心亭（市定古蹟）',
-            address='新興里 台中市北區',
-            latitude=24.144433941595807,
-            longitude=120.68420319880194
+            title='熱門景點',
+            address='Skarðsáfossur',
+            latitude=62.09834026797917,
+            longitude=-7.406501774391917
         )
             line_bot_api.reply_message(event.reply_token, location_message)
 
     elif message == '熱門音樂':
             audio_message = AudioSendMessage(
-            original_content_url='https://youtu.be/Vk5-c_v4gMU?si=QQe0z7XjniwTTzup',  # 替換為實際的音樂檔案網址
+            original_content_url='https://youtu.be/sJ-2X3rHtXw?si=Ow7QJEA94g_oSzjo',  # 替換為實際的音樂檔案網址
             duration=240000  # 音樂長度（毫秒）
         )
             line_bot_api.reply_message(event.reply_token, audio_message)
 
     elif message == '放鬆音樂':
             audio_message = AudioSendMessage(
-            original_content_url='https://youtu.be/MZ8Ri-_Uusk?si=RCQEDdif3Q5ciZLY',  # 替換為實際的音樂檔案網址
+            original_content_url='https://youtu.be/b5d5OmmUlPc?si=KjqoCFqC-zMPUbBU',  # 替換為實際的音樂檔案網址
             duration=300000  # 音樂長度（毫秒）
         )
             line_bot_api.reply_message(event.reply_token, audio_message)
 
     elif message == '今天是我的生日':
             image_message = ImageSendMessage(
-            original_content_url='https://drive.google.com/file/d/1gvVZB4XsSYK8pgAZhu9_hWzARZVbvn8B/view?usp=sharing',  # 替換為實際的圖片網址
-            preview_image_url='https://drive.google.com/file/d/1gvVZB4XsSYK8pgAZhu9_hWzARZVbvn8B/view?usp=sharing'  # 替換為實際的預覽圖片網址
+            original_content_url='https://instagram.frmq1-1.fna.fbcdn.net/v/t51.29350-15/445982392_1168493424581932_2823979820016375869_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi44MDB4NTk5LnNkci5mMjkzNTAuZGVmYXVsdF9pbWFnZSJ9&_nc_ht=instagram.frmq1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=sB_C31Fv8iEQ7kNvgHhyc2m&_nc_gid=e634efac047d43908457cd99e591ae05&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzM3MzY4MDM2NzUyMDk1MzkwMw%3D%3D.3-ccb7-5&oh=00_AYBT_hpWmBXVyHjuu3wIyJ9sU2H78qC0k1msekBCMXYG0w&oe=675A9D7C&_nc_sid=10d13b',  # 替換為實際的圖片網址
+            preview_image_url='https://instagram.frmq1-1.fna.fbcdn.net/v/t51.29350-15/445982392_1168493424581932_2823979820016375869_n.jpg?stp=dst-jpg_e35_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi44MDB4NTk5LnNkci5mMjkzNTAuZGVmYXVsdF9pbWFnZSJ9&_nc_ht=instagram.frmq1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=sB_C31Fv8iEQ7kNvgHhyc2m&_nc_gid=e634efac047d43908457cd99e591ae05&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzM3MzY4MDM2NzUyMDk1MzkwMw%3D%3D.3-ccb7-5&oh=00_AYBT_hpWmBXVyHjuu3wIyJ9sU2H78qC0k1msekBCMXYG0w&oe=675A9D7C&_nc_sid=10d13b'  # 替換為實際的預覽圖片網址
         )
             text_message = TextSendMessage(text='生日快樂！')
             line_bot_api.reply_message(event.reply_token, [image_message, text_message])
@@ -105,7 +105,9 @@ def handle_message(event):
     elif message in ['動作片', '動畫', '紀錄片']:
         # 根據類型傳送影片
         video_urls = {
-            '動畫': 'https://www.youtube.com/watch?v=1-hNreqVpfA'
+            '動作片': 'https://youtu.be/6PP7QzMowp4?si=JsEKHmvDJugizqzH',
+            '動畫': 'https://youtu.be/tfHhtCSGzn0?si=vjxdRSGM8ewilx0_',
+            '紀錄片': 'https://youtu.be/vmnuj5SoG-o?si=9uGkb1E_ZPb0Fxre'
         }
         video_url = video_urls.get(message)
         if video_url:
